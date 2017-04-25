8f3ca2b13f4ab4449c40740f56d95f098bf9689c
Rishi Raje <hrishiraje@gmail.com>
2017-04-25 11:21:32 -0700

    Initial commit Will set up all package.json dependencies and a basic server

---
+++ server/app.js
@@ -0,0 +1,30 @@
+var cors = require('cors');
+var express = require('express');
+var parser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var partials = require('express-partials');
+// var router = require('./routers/router');
+var path = require('path');
+
+var app = express();
+app.set('port', 9000);
+
+app.use(cors());
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(partials());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.use(router);
+
+app.get('*', function(request, response){
+  response.sendFile(path.join(__dirname, '../public/index.html'));
+});
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Server listening on port '+app.get('port'));
+}
+
+module.exports.app = app;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        exclude: '/node_modules/',
+        test: /\.js$/
+      }
+    ]
+  }
+};
+

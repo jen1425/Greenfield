6f800bea87a7d3ba89472c39ea6db2b9586d7918
Aaron Xavier <aarondxavier@gmail.com>
2017-05-05 18:32:22 -0700

    With first working test!

--- spec/client/components/App.test.js
+++ spec/client/components/App.test.js
@@ -5,17 +5,17 @@ import App from '../../../client/components/app';
 describe('<App />', () => {
   it('should render one <Nav /> component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Nav)).to.have.length(1);
+    expect(wrapper.find('Nav').length).toBe(1);
   });
 
   it('should render one <TrackList /> component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(TrackList)).to.have.length(1);
+    expect(wrapper.find('TrackList').length).toBe(1);
   });
 
   it('should render one <Controls /> component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Controls)).to.have.length(1);
+    expect(wrapper.find('Controls').length).toBe(1);
   });
 
 });

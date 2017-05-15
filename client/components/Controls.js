dfb22e3c56615a060b7deb578719d3bba98d7e64
jen1425 <jenniferhwa@gmail.com>
2017-05-15 15:40:04 -0700

    Styling and Layout updates

--- client/components/Controls.js
+++ client/components/Controls.js
@@ -27,15 +27,6 @@ class Controls extends React.Component {
 
   render() {
 
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">Panel title</h3>
-  </div>
-  <div class="panel-body">
-    Panel content
-  </div>
-</div>
-
     return (
       <div className='controls'>
         <Search submitHandler={this.props.submitHandler} />
